31f2b3ef2fe12657784766f6b59dd61bb1cf75b3
JohnVow <john95web@gmail.com>
2021-12-09 13:02:38 +0200

    First:Wnter+Plugins for 7.vdomah.rolesbuddies

---
+++ plugins/vdomah/rolesbuddies/updates/buddies_add_role_id_column.php
@@ -0,0 +1,37 @@
+<?php namespace Vdomah\RolesBuddies\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class UpdateBuddiesAddRoleIdColumn extends Migration
+{
+    public $table = 'lovata_buddies_users';
+
+    public $column = 'vdomah_role_id';
+
+    public function up()
+    {
+        if (!Schema::hasTable($this->table) ||
+            Schema::hasColumn($this->table, $this->column)
+        )
+            return;
+
+        Schema::table($this->table, function($table)
+        {
+            $table->integer($this->column)->nullable();
+        });
+    }
+
+    public function down()
+    {
+        if (!Schema::hasTable($this->table) ||
+            !Schema::hasColumn($this->table, $this->column)
+        )
+            return;
+
+        Schema::table($this->table, function($table)
+        {
+            $table->dropColumn($this->column);
+        });
+    }
+}
